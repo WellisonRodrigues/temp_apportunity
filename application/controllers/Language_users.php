7d34354880aa9759796d4c079b026c445b4b76e9
Wellison <wellison.244@hotmail.com>
2017-10-28 19:41:27 -0200

    users

--- application/controllers/Language_users.php
+++ application/controllers/Language_users.php
@@ -1,11 +1,13 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Wellison
  * Date: 28/10/2017
  * Time: 18:05
  */
-class Language_users extends CI_Controller {
+class Language_users extends CI_Controller
+{
 
     public function get_profile_language()
     {
@@ -58,6 +60,64 @@ class Language_users extends CI_Controller {
         $resp['headers'] = $headers;
         $resp['err'] = $err;
         return $resp;
+    }
+
+    public function create_languages($name, $level)
+    {
+
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+//        $type = $this->session->userdata("logado")->type;
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => "http://34.229.150.76:3000/api/v1/profile/languages",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\n  \"data\": {\n    \"type\": \"languages\",\n  
+              \"attributes\": {\n      \"name\": \"$name\",\n      \"level\": \"$level\"\n    }\n  }\n}",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "content-type: application/vnd.api+json",
+                "postman-token: d9dcab3a-4a79-c78f-12e6-d6ab280fb5ca",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+
     }
 
 }
