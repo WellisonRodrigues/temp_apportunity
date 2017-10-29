fd70527bd0fc194adbf0033264e4ab5b15037731
Wellison <wellison.244@hotmail.com>
2017-10-29 21:48:33 -0200

    vagassaved list

--- application/controllers/Vagas.php
+++ application/controllers/Vagas.php
@@ -1,18 +1,32 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Wellison
  * Date: 29/10/2017
  * Time: 18:30
  */
-class Vagas extends CI_Controller{
+class Vagas extends CI_Controller
+{
 
-    public function index(){
-
+    public function index()
+    {
 
 
         $retorno = $this->get_vagas_salvas();
-        $data ['vagas_salvas'] = $retorno['response']['data'];
+//        $job_salvo = $this->get_job_salvo($idjob);
+        $saved_jobs ['vagas_salvas'] = $retorno['response']['data'];
+        foreach ($saved_jobs['vagas_salvas'] as $vaga_salva) {
+
+//            print_r($vaga_salva['relationships']['job']['data']['id']);
+
+            $job_salvo = $this->get_job_salvo($vaga_salva['relationships']['job']['data']['id']);
+            $jobs[] = $job_salvo['response'];
+//            print_r($jobs['jobs']['response']);
+        }
+//        die;
+        $data['jobs'] = $jobs;
+//        $job_salvo = $this->get_job_salvo($idjob);
         $data['view'] = 'forms/vagas_list';
         $this->load->view('template_admin/core', $data);
     }
@@ -87,4 +101,54 @@ class Vagas extends CI_Controller{
         return $array;
     }
 
+    private function get_job_salvo($idjob)
+    {
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => "http://34.229.150.76:3000/api/v1/jobs/$idjob",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "postman-token: 94c50fec-c163-3e33-fe9f-2211e7fe3711",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+    }
 }
--- application/views/forms/vagas_list.php
+++ application/views/forms/vagas_list.php
@@ -5,10 +5,43 @@
  * Date: 29/10/2017
  * Time: 18:32
  */
-//print_r($vagas_salvas);
+//print_r($jobs);
 
-foreach ($vagas_salvas as $vaga_salva){
+foreach ($jobs as $job_salvo) {
 
-    print_r($vaga_salva['relationships']['job']);
-}
-?>
+//    ($job_salvo['data']['attributes']['title']);
+
+    ?>
+
+    <div class="col-md-6 col-md-offset-3 col-sm-offset-1 col-sm-10">
+        <article>
+
+            <div class="social-feed-box">
+                <div class="social-avatar">
+                    <a href="" class="pull-left">
+                        <img alt="image" src="http://webapplayers.com/inspinia_admin-v2.5/img/a1.jpg">
+                    </a>
+                    <div class="media-body">
+                        <a href="#">
+                            <?php
+                            echo $name;
+                            ?>
+                        </a>
+                        <small class="text-muted"></small>
+                    </div>
+                </div>
+                <div class="social-body">
+                    <h4> <?php echo $job_salvo['data']['attributes']['title'] ?></h4>
+                    <p>
+                        <?php echo $job_salvo['data']['attributes']['description'] ?>
+                        <br>
+                    </p>
+
+                </div>
+            </div>
+
+
+        </article>
+    </div>
+
+<?php } ?>
