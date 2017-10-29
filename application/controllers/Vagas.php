b13c0215c1ad40a761a9709ae9a3fbd9933ff203
mvcampos <vinicius@mvcampos.com.br>
2017-10-29 19:16:53 -0200

    correcao

---
+++ application/controllers/Vagas.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wellison
+ * Date: 29/10/2017
+ * Time: 18:30
+ */
+class Vagas extends CI_Controller{
+
+    public function index(){
+
+
+
+        $retorno = $this->get_vagas_salvas();
+        $data ['vagas_salvas'] = $retorno['response']['data'];
+        $data['view'] = 'forms/vagas_list';
+        $this->load->view('template_admin/core', $data);
+    }
+
+
+    private function get_vagas_salvas()
+    {
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => "http://34.229.150.76:3000/api/v1/users/saved_jobs",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "postman-token: 867d450e-ae18-0a3c-2ca0-055e026cb8b2",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+    }
+
+
+    public function arrayCastRecursive($array)
+    {
+        if (is_array($array)) {
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $array[$key] = $this->arrayCastRecursive($value);
+                }
+                if ($value instanceof stdClass) {
+                    $array[$key] = $this->arrayCastRecursive((array)$value);
+                }
+            }
+        }
+        if ($array instanceof stdClass) {
+            return $this->arrayCastRecursive((array)$array);
+        }
+        return $array;
+    }
+
+}
