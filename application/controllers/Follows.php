38f642513b13a68cbc8bc9f9ed38aceac4501613
Wellison <wellison.244@hotmail.com>
2017-11-19 21:56:35 -0200

    seguidores

--- application/controllers/Follows.php
+++ application/controllers/Follows.php
@@ -147,6 +147,115 @@ class Follows extends CI_Controller
 
     }
 
+    public function create_follows()
+    {
+
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+//    $type = $this->session->userdata("logado")->type;
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => "http://34.229.150.76:3000/api/v1/users/follows",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "postman-token: de96bd2e-2ecb-1bf6-1ab5-4b757b2e977b",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+
+    }
+
+    private function create_follows_ws($id_follow)
+    {
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+//    $type = $this->session->userdata("logado")->type;
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => "http://34.229.150.76:3000/api/v1/users/follows",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\r\n  \"data\": {\r\n    \"type\": \"follows\",
+            \r\n    \"relationships\": {\r\n        \"followed\": {\r\n        \"data\": {\r\n          \"type\": \"users\",
+            \r\n          \"id\": \"$id_follow\"\r\n        }\r\n      }\r\n    }\r\n  }\r\n}",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "content-type: application/vnd.api+json",
+                "postman-token: 4c5852d3-b688-5226-5204-64670b8ebd77",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+
+    }
+
     public function get_users_list($id_user)
     {
         $retorno_follows = $this->get_follows_ws($id_user);
--- application/views/forms/conexoes_list.php
+++ application/views/forms/conexoes_list.php
@@ -27,7 +27,42 @@
 
                 <div class="panel panel-body">
                     <div class="tab-content">
-                        <div class="tab-pane active" id="tab_seguidores">
+                        <div class="tab-pane active" id="tab_seguindo">
+                            <div class="text-left">
+                                <h3>Seguindo</h3>
+                            </div>
+                            <div class="row">
+                                <?php foreach ($followers as $follow_as) {
+//                                   <?php foreach ($followed as $follow_wed) {
+                                    ?>
+
+                                    <script>
+                                        $(document).ready(function () {
+                                            var iduser = "<?php echo $follow_as; ?>";
+
+                                            function list_user_f() {
+                                                $.get("<?php echo base_url('Follows/get_users_list/')?>" + iduser,
+
+                                                    function (result) {
+                                                        $("#result_follower" + iduser).html(result);
+
+                                                    }
+                                                )
+                                            }
+
+                                            //LISTAR
+                                            list_user_f();
+                                        });
+                                    </script>
+                                    <div id="result_follower<?php echo $follow_as; ?>">
+
+                                    </div>
+
+
+                                <?php } ?>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab_seguidores">
                             <div class="text-left">
                                 <h3>Seguidores</h3>
                             </div>
@@ -55,41 +90,6 @@
                                         });
                                     </script>
                                     <div id="result_followed<?php echo $follow_wed; ?>">
-
-                                    </div>
-
-
-                                <?php } ?>
-                            </div>
-                        </div>
-                        <div class="tab-pane" id="tab_seguindo">
-                            <div class="text-left">
-                                <h3>Seguindo</h3>
-                            </div>
-                            <div class="row">
-                                <?php foreach ($followers as $follow_as) {
-//                                    print_r($followers);
-                                    ?>
-
-                                    <script>
-                                        $(document).ready(function () {
-                                            var iduser = "<?php echo $follow_as; ?>";
-
-                                            function list_user_f() {
-                                                $.get("<?php echo base_url('Follows/get_users_list/')?>" + iduser,
-
-                                                    function (result) {
-                                                        $("#result_follower" + iduser).html(result);
-
-                                                    }
-                                                )
-                                            }
-
-                                            //LISTAR
-                                            list_user_f();
-                                        });
-                                    </script>
-                                    <div id="result_follower<?php echo $follow_as; ?>">
 
                                     </div>
 
