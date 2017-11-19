24268b45a708dafcf06a6e09b96db8b9c36b7807
Wellison <wellison.244@hotmail.com>
2017-11-19 15:12:18 -0200

    seguidores

--- application/views/forms/conexoes_list.php
+++ application/views/forms/conexoes_list.php
@@ -15,31 +15,48 @@
             <div class="panel panel-default">
                 <div class="panel panel-heading">
                     <div class="row">
-                        <div class="col-md-6 text-center"><h3>Seguindo <br> <?php echo @$seguindo ?></h3></div>
-                        <div class="col-md-6 text-center"><h3>Seguidores<br> <?php echo @$inscritos ?></h3></div>
+                        <div class="col-md-6 text-center">
+                            <h3><a href="#tab_seguindo" data-toggle="tab"
+                                   id="li_seguindo">Seguindo <br> <?php echo @$seguindo ?></a></h3></div>
+                        <div class=" col-md-6 text-center">
+                            <h3><a href="#tab_seguidores" data-toggle="tab"
+                                   id="li_seguidores">Seguidores<br> <?php echo @$inscritos ?></a></h3>
+                        </div>
                     </div>
                 </div>
-                <div class="panel panel-body">
-                    <div class="row">
-                        <?php foreach ($followed as $follow_wed) { ?>
 
-                            <div class="col-md-6">
-                                <img src="<?php echo base_url(IMAGES); ?>/profile.jpg" class="img-rounded" width="15%"
-                                     height="15%">
+                <div class=" panel panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab_seguidores">
+                            <div class="row">
+                                <?php foreach ($followed as $follow_wed) { ?>
+
+                                    <div class="col-md-6">
+                                        <img src="<?php echo base_url(IMAGES); ?>/profile.jpg"
+                                             class="img-rounded"
+                                             width="15%"
+                                             height="15%">
+                                    </div>
+
+                                <?php } ?>
                             </div>
+                        </div>
+                        <br>
+                        <div class="tab-pane" id="tab_seguindo">
+                            <div class="row">
+                                <?php foreach ($followers as $follow_as) { ?>
 
-                        <?php } ?>
-                    </div>
-                    <br>
-                    <div class="row">
-                        <?php foreach ($followers as $follow_as) { ?>
+                                    <div class="col-md-6">
+                                        <img src="<?php echo base_url(IMAGES); ?>/profile.jpg"
+                                             class="img-rounded"
+                                             width="15%"
+                                             height="15%">
+                                    
+                                    </div>
 
-                            <div class="col-md-6">
-                                <img src="<?php echo base_url(IMAGES); ?>/profile.jpg" class="img-rounded" width="15%"
-                                     height="15%">
+                                <?php } ?>
                             </div>
-
-                        <?php } ?>
+                        </div>
                     </div>
                 </div>
             </div>
