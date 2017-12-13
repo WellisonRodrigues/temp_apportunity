a1a54d61f486cd9753d031f5eb0643048e1ce54f
milton.vinicius <milton.vinicius@avantibr.com.br>
2017-12-13 17:50:53 -0200

    lista pesquisa

--- application/controllers/Pesquisar.php
+++ application/controllers/Pesquisar.php
@@ -1,18 +1,163 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Wellison
  * Date: 21/11/2017
  * Time: 00:00
  */
-class Pesquisar extends CI_Controller{
+class Pesquisar extends CI_Controller
+{
 
-    public function index(){
-
+    public function index()
+    {
         $data['view'] = 'forms/pesquisar_filtro';
         $this->load->view('template_admin/core', $data);
+    }
+
+    public function listar()
+    {
+        $type = $this->input->post('type');
+        $regiao = $this->input->post('regiao');
+
+        if ($type == 'users') {
+            $retorno = $this->listar_ws_users($regiao);
+        } elseif ($type == 'companies') {
+            $retorno = $this->listar_ws_companies($regiao);
+        }
+
+        if (isset($retorno["err"]) && !empty($retorno["err"])) {
+            $data['alert'] =
+                [
+                    'type' => 'erro',
+                    'message' => 'Problemas no servidor. Entrar contato com a equipe de ti.'
+                ];
+            $this->session->set_flashdata('alert', $data['alert']);
+            //redirect('Perfil_user');
+        }
+        print_r($retorno["response"]);
+        if (!empty($retorno['response']['data'])) {
+            echo json_encode($retorno['response']['data']);
+        }
+        /*$data['view'] = 'forms/pesquisar_filtro';
+        $this->load->view('template_admin/core', $data);*/
 
     }
 
+    private function listar_ws_users($regiao)
+    {
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => 'http://34.229.150.76:3000/api/v1/users?filter[region]={' . urlencode($regiao) . '}',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "postman-token: c8ded2be-172d-f095-775f-3cfda71520cf",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+    }
+
+    private function listar_ws_companies($regiao)
+    {
+        $aut_code = $this->session->userdata('verify')['auth_token'];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "3000",
+            CURLOPT_URL => 'http://34.229.150.76:3000/api/v1/companies?filter[region]={' . urlencode($regiao) . '}',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/vnd.api+json",
+                "cache-control: no-cache",
+                "postman-token: c8ded2be-172d-f095-775f-3cfda71520cf",
+                "x-auth-token: $aut_code"
+            ),
+        ));
+
+        $headers = [];
+        curl_setopt($curl, CURLOPT_HEADERFUNCTION,
+            function ($curl, $header) use (&$headers) {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $headers))
+                    $headers[$name] = [trim($header[1])];
+                else
+                    $headers[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+        $response = curl_exec($curl);
+        $resposta = json_decode($response);
+        $err = curl_error($curl);
+        curl_close($curl);
+        $array = $this->arrayCastRecursive($resposta);
+        $resp['response'] = $array;
+        $resp['headers'] = $headers;
+        $resp['err'] = $err;
+        return $resp;
+    }
+
+    public function arrayCastRecursive($array)
+    {
+        if (is_array($array)) {
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $array[$key] = $this->arrayCastRecursive($value);
+                }
+                if ($value instanceof stdClass) {
+                    $array[$key] = $this->arrayCastRecursive((array)$value);
+                }
+            }
+        }
+        if ($array instanceof stdClass) {
+            return $this->arrayCastRecursive((array)$array);
+        }
+        return $array;
+    }
 
 }
--- application/views/forms/pesquisar_filtro.php
+++ application/views/forms/pesquisar_filtro.php
@@ -5,28 +5,56 @@
  * Date: 21/11/2017
  * Time: 00:01
  */
-
+echo base_url();
 ?>
-
-    <div class="col-md-8 col-md-offset-2 col-sm-offset-1 col-sm-10">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="form-group">
-                    <select class="form-control" id="sel1" name="type" onchange="yesnoCheck(this);">
-                        <option id="companies" value="companies">Empresa</option>
-                        <option id="users" value="users">Estabelecimento</option>
-                        <option id="users" value="users">Pessoa Fisica</option>
-                    </select>
-                </div>
-                </div>
-            <div class="col-lg-6">
-                <div class="input-group">
-                    <input type="text"class="form-control" placeholder="Região">
+<div class="col-md-8 col-md-offset-2 col-sm-offset-1 col-sm-10">
+    <div class="row">
+        <?php echo form_open('Pesquisar/listar', array('id'=>'formulario','method'=>'post','role' => 'form'));
+        ?>
+        <div class="col-lg-6">
+            <div class="form-group">
+                <select class="form-control" id="type" name="type">
+                    <option id="companies" value="companies">Empresa</option>
+                    <!--<option id="users" value="users">Estabelecimento</option>-->
+                    <option id="users" value="users">Pessoa Fisica</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="input-group">
+                <input type="text" class="form-control" id="regiao" name="regiao" placeholder="Região (São Paulo, Belo Horizonte , Rio de Janeiro)">
                     <span class="input-group-btn">
-                        <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                        <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
                   </span>
-                </div><!-- /input-group -->
-            </div><!-- /.col-lg-6 -->
-        </div><!-- /.row -->
+            </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+        <?php echo form_close() ?>
+    </div><!-- /.row -->
+
+    <div class="row">
+        <div class="social-feed-box">
+            <div class="social-avatar">
+                <a href="" class="pull-left">
+                    <img alt="image" src="http://webapplayers.com/inspinia_admin-v2.5/img/a1.jpg">
+                </a>
+                <div class="media-body">
+                    <a href="#">
+                        NOME
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<script>
+    $(function () {
+        $("form#formulario").submit(function(e) {
+            var type = $("#type").val();
+            var regiao = $("#regiao").val();
+            $.post('<?php echo base_url()?>Pesquisar/listar', {regiao:regiao,type:type},function(data){
+            })
+            return false;
+        })
+    })
+</script>
 </div>
