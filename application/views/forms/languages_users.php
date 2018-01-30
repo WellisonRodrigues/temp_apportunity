c962cc8805a7b0450ed11c06ae84b58f49c3aafb
milton.vinicius <milton.vinicius@avantibr.com.br>
2018-01-30 14:13:13 -0200

    + habilidades

--- application/views/forms/languages_users.php
+++ application/views/forms/languages_users.php
@@ -26,9 +26,59 @@
                            required>
                 </div>
                 <div class="form-group">
-                    <input class="form-control" placeholder="Level" type="text" name="level" autofocus
+                    <select class="form-control" name="level" required>
+                        <option value="">-- Selecione o level --</option>
+                        <option value="basic">Basic</option>
+                        <option value="intermediate">Intermediate</option>
+                        <option value="advanced">Advanced</option>
+                        <option value="fluent">Fluent</option>
+                        <option value="native">Native</option>
+                    </select>
+                </div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <input type="submit" class="btn btn-primary" name="salvar"
+                       value="Salvar">
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <?php echo form_close() ?>
+    <!-- /.modal-dialog -->
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="new_habilidades" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+     aria-hidden="true"
+     style="display: none;">
+    <div class="modal-dialog">
+        <?php
+        echo form_open('Perfil_user/create_habilidades', ['role' => 'form']);
+        ?>
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Nova Habilidade</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <input class="form-control" placeholder="Habilidade" type="text" name="name_habilidades" autofocus
                            value="" autocomplete="off"
                            required>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" name="level" required>
+                        <option value="">-- Selecione o level --</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
                 </div>
 
 
@@ -55,7 +105,7 @@
     <br>
     <hr>
     <div class="text-right">
-        <button type="button" class="btn btn-primary small" data-toggle="modal"
+        <button type="button" class="btn btn-primary small" data-toggle="modal"  title="Cadastrar Idioma" style="margin:2px 0px"
                 data-target="#new_languages"><i class="fa fa-plus"></i></button>
     </div>
     <?php foreach ($idiomas as $idioma) { ?>
@@ -70,9 +120,9 @@
             <div class="col-md-2">
 
                 <button type="button" class="btn btn-primary"><i
-                            style="text-decoration: none;color: white"
-                            class="fa fa-pencil" data-toggle="modal"
-                            data-target=".edit_languages<?php echo $idioma['id'] ?>"></i>
+                        style="text-decoration: none;color: white"
+                        class="fa fa-pencil" data-toggle="modal"
+                        data-target=".edit_languages<?php echo $idioma['id'] ?>"></i>
                 </button>
                 <button type="button"
                         class="btn btn-danger"> <?php echo anchor('Perfil_user/delete_idioma/' . $idioma['id'],
@@ -104,19 +154,93 @@
                                    required readonly>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" placeholder="Level" type="text" name="level" autofocus
-                                   value="<?php echo $idioma['attributes']['level'] ?>" autocomplete="off"
-                                   required>
+                            <select class="form-control" name="level" required>
+                                <option value="">-- Selecione o level --</option>
+                                <option value="basic" <?php if($idioma['attributes']['level'] == 'basic') echo "SELECTED"; ?>>Basic</option>
+                                <option value="intermediate" <?php if($idioma['attributes']['level'] == 'intermediate') echo "SELECTED"; ?>>Intermediate</option>
+                                <option value="advanced" <?php if($idioma['attributes']['level'] == 'advanced') echo "SELECTED"; ?>>Advanced</option>
+                                <option value="fluent" <?php if($idioma['attributes']['level'] == 'fluent') echo "SELECTED"; ?>>Fluent</option>
+                                <option value="native" <?php if($idioma['attributes']['level'] == 'native') echo "SELECTED"; ?>>Native</option>
+                            </select>
                         </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Level" type="hidden" name="id" autofocus
-                                   value="<?php echo $idioma['id'] ?>" autocomplete="off"
-                                   required>
-                        </div>
-
-
                     </div>
                     <div class="modal-footer">
+                        <input type="hidden" name="id" value="<?php echo $idioma['id'] ?>" />
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <input type="submit" class="btn btn-primary" name="editar"
+                               value="Editar">
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <?php echo form_close() ?>
+            <!-- /.modal-dialog -->
+        </div>
+
+    <?php } ?>
+
+    <div class="text-right">
+        <button type="button" class="btn btn-primary small" data-toggle="modal" title="Cadastrar Habilidades" style="margin:2px 0px"
+                data-target="#new_habilidades"><i class="fa fa-plus"></i></button>
+    </div>
+
+    <?php foreach ($habilidades as $habilidade) { ?>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-2">
+                <div class="text-right">
+                    <h4><strong>Habilidades <?php echo $habilidade['attributes']['name'] ?>
+                        </strong></h4>
+                </div>
+            </div>
+            <div class="col-md-2">
+
+                <button type="button" class="btn btn-primary"><i
+                        style="text-decoration: none;color: white"
+                        class="fa fa-pencil" data-toggle="modal"
+                        data-target=".edit_habilidade<?php echo $habilidade['id'] ?>"></i>
+                </button>
+                <button type="button"
+                        class="btn btn-danger"> <?php echo anchor('Perfil_user/delete_habilidade/' . $habilidade['id'],
+                        '<i
+                            style="text-decoration: none;color: white" class="fa fa-trash"> </i>') ?></button>
+
+
+            </div>
+        </div>
+        Level : <?php echo $habilidade['attributes']['level'] ?><br>
+
+        <div class="modal fade edit_habilidade<?php echo $habilidade['id'] ?>" tabindex="-1" role="dialog"
+             aria-labelledby="myModalLabel"
+             aria-hidden="true"
+             style="display: none;">
+            <div class="modal-dialog">
+                <?php
+                echo form_open('Perfil_user/edit_habilidade', ['role' => 'form']);
+                ?>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title" id="myModalLabel">Editar Habilidade</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Idioma" type="text" name="name" autofocus
+                                   value="<?php echo $habilidade['attributes']['name'] ?>" autocomplete="off"
+                                   required readonly>
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" name="level" required>
+                                <option value="">-- Selecione o level --</option>
+                                <option value="1" <?php if($habilidade['attributes']['level'] == '1') echo "SELECTED"; ?>>1</option>
+                                <option value="2" <?php if($habilidade['attributes']['level'] == '2') echo "SELECTED"; ?>>2</option>
+                                <option value="3" <?php if($habilidade['attributes']['level'] == '3') echo "SELECTED"; ?>>3</option>
+                                <option value="4" <?php if($habilidade['attributes']['level'] == '4') echo "SELECTED"; ?>>4</option>
+                                <option value="5" <?php if($habilidade['attributes']['level'] == '5') echo "SELECTED"; ?>>5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" name="id" value="<?php echo $habilidade['id'] ?>" />
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
                         <input type="submit" class="btn btn-primary" name="editar"
                                value="Editar">
